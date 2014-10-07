f4dd20ed7f679a978c21e29f71c9f09a830f4f41
Joao Sa <me@joaomsa.com>
2014-10-07 00:36:21 -0300

    Don't run script on useless Facebook iframes

--- dist/botman.user.js
+++ dist/botman.user.js
@@ -3,8 +3,11 @@
 // @namespace   botman
 // @description Botman is here to speed up those witty responses on facebook
 // @include     https://www.facebook.com/*
-// @version     1.1.3
+// @exclude     https://www.facebook.com/ai.php*
+// @exclude     https://www.facebook.com/xti.php*
+// @version     1.1.4
 // @grant       GM_xmlhttpRequest
+// @run-at      document-end
 // ==/UserScript==
 
 var robot = new Botman("botman");
